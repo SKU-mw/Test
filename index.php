ddc23c32bfb32d19cf25c00b3373bb17320db544
BR-mw <137156572+BR-mw@users.noreply.github.com>
2023-06-20 10:14:51 +0200

    Update index.php
    PHP Tags

--- index.php
+++ index.php
@@ -4,18 +4,27 @@
 
 <h1>Hello World!</h1>
 <h2>Post Vars</h2>
-  echo "<pre>";
+ <?php
+	
+	echo "<pre>";
 	print_r($_POST);
 	echo "</pre>";
+?>
+
 <h3>Get Vars</h3>
-  echo "<pre>";
+<?php
+
+echo "<pre>";
 	print_r($_GET);
 	echo "</pre>";
+?>
 <h4>Server Environment</h4>
-echo "<pre>";
+<?php
+
+	echo "<pre>";
 	print_r($_SERVER);
 	echo "</pre>";
-  
+?>  
 <h5>Heading 5</h5>
 <h6>Heading 6</h6>
 
